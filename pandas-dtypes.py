773f58b1bf24e1ea0c592b4968e30cf8c65df498
Esuabom Dijemeni <e.dijemeni@gmail.com>
2023-01-10 14:51:27 +0000

    Understanding the data types

---
+++ pandas-dtypes.py
@@ -0,0 +1,18 @@
+# module 
+import mysql.connector 
+import pandas as pd
+
+conn = mysql.connector.connect(read_default_file = '/Users/lotannadijemeni/.my.cnf')
+
+query = '''
+SELECT title, genre, avg_vote
+FROM `oscarval_sql_course`.`imdb_movies`
+LIMIT 5
+'''
+
+df = pd.read_sql(query, conn)
+print(df.head())
+
+print(df.dtypes)
+
+conn.close()
