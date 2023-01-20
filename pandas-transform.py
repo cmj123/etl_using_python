07bbdc53630dd8c847be52ede4364b11f6ef1c55
Esuabom Dijemeni <e.dijemeni@gmail.com>
2023-01-20 20:31:30 +0000

    Completed data transform for city_housing dataset

---
+++ pandas-transform.py
@@ -0,0 +1,27 @@
+# modules 
+import mysql.connector
+import pandas as pd
+import os 
+
+conn = mysql.connector.connect(read_default_file = '/Users/lotannadijemeni/.my.cnf')
+
+query = '''
+SELECT *
+FROM `oscarval_sql_course`.`city_house_prices`
+'''
+
+df = pd.read_sql(query, conn)
+
+# data tranformation steps
+df.set_index('Date', inplace=True)
+df = df.stack().reset_index()
+
+df.columns = ['date', 'city', 'price']
+df[['state','city']] = df['city'].str.split('-',expand=True)
+df = df[['date','state','city','price']]
+
+df.to_csv('city_housing.csv', index=False)
+
+print(df.head())
+
+conn.close()
